430ab690a994d721d6539e2534da63086356db1b
Osam k <74204122+OSAMA263@users.noreply.github.com>
2023-06-26 14:17:39 +0300

    Update Loader.jsx

--- src/components/Features/Loader.jsx
+++ src/components/Features/Loader.jsx
@@ -1,9 +1,8 @@
-import img from "loader.gif"
 
 export default function Loader() {
   return (
     <div className="flex h-[100vh] items-center justify-center bg-white text-center">
-      <img src={img} alt="" />
+      <img src="loader.gif" alt="" />
     </div>
   );
 }
