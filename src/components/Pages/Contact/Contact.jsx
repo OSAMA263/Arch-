d3688336cf640a83c8d24fdbfc553e96f87c9924
Osam k <74204122+OSAMA263@users.noreply.github.com>
2023-07-06 11:23:34 +0300

    Update Contact.jsx

--- src/components/Pages/Contact/Contact.jsx
+++ src/components/Pages/Contact/Contact.jsx
@@ -34,14 +34,14 @@ export default function Contact() {
         text="We’d love to hear more about your project. Please, leave a message below or give us a call. We have two offices, one in Texas and one in Tennessee. If you find yourself nearby, come say hello!"
       />
       <ContactDetails />
-      <div className="my-32 flex justify-center">
+      <section id="map" className="my-32 flex justify-center">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3113.8066764930795!2d178.05489017683487!3d-38.699284785166235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d66790cd3c4c247%3A0xb95f65c1e03ea535!2sSponge%20Bay%20Beach!5e0!3m2!1sen!2seg!4v1687862553877!5m2!1sen!2seg"
           width="600"
           height="450"
           loading="lazy"
         ></iframe>
-      </div>
+      </section>
     </motion.section>
   );
 }
